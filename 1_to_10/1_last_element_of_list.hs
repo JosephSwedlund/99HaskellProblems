7b42ce43e1d6b3c06b0f60afc5e05ad83d28d51c
Joseph Swedlind <JosephSwedlund@gmail.com>
2022-03-20 23:08:42 -0500

    Solved problem 1 :)

--- 1_to_10/1_last_element_of_list.hs
+++ 1_to_10/1_last_element_of_list.hs
@@ -1,5 +1,7 @@
-find_last_element list[] =
-    if null tail:list
-    then list
-    else find_last_element tail:list
+
+
+find_last_element :: [a] -> a
+find_last_element [x] = x
+find_last_element (_:list) =
+    find_last_element list
 
